d566cf414afeafef8c91bc9f394677981a0cc55e
jmaleckaite <justina.maleckaite13@gmail.com>
2020-10-13 09:59:54 +0200

    Update

--- src/PopUp.js
+++ src/PopUp.js
@@ -1,64 +1,129 @@
 import React, { Component } from "react";
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Header, Icon, Modal, Message } from 'semantic-ui-react';
+import { Button, Header, Icon, Modal, Message, Checkbox } from 'semantic-ui-react';
 
-function PopUp() {
-  const [open, setOpen, saved, setSaved] = React.useState(false)
+
+/**
+interface IState {
+	ch1: boolean;
+	ch2: boolean;
+	ch3: boolean;
+}
+
  
-  
   function handleSubmit(e) {
     e.preventDefault();
-
     setSaved(true);
   }
+  
+  function handleCheck(e) {
+	  console.log("test", e);
+  };
+  
+export default class PopUp extends React.Component {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			ch1: true,
+			ch2: true,
+			ch3: true
+		};
+	}
+	
+	checkboxChangeHandler = (event: React.FormEvent<HTMLInputElement>, data) => {
+		const value = data.checked;
+		console.log(data);
+		this.setState({ [data.name]: data.checked });
+		console.log(value);
+	};
+	
+	render() {**/
+	function PopUp() {
+		
+		const [open, setOpen, saved, setSaved] = React.useState(false);
+		const [ch1, ch2, ch3] = useState(false); //use boolean value
+		
+		//add a state checked false, and user click events
+		const state = { checked: false }
+		
 
+		function onChangeCheckbox(data) {
+			let checked = data.checked;
+			console.log(checked);
+		}
+		
+		function handleSubmit(e) {
+			e.preventDefault();
+			setSaved(true);
+		}
+		
+		
+	
+		
     return (
+	<div className="wrapper">
 	 <Modal
       closeIcon
       open={open}
-      trigger={<Button color='blue'>Subscribe</Button>}
+      trigger={<Button color='blue' style={{position: "center"}} >Subscribe</Button>}
       onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
 	  onSubmit={e => handleSubmit(e)}
+	  size='small'
     >
       <Header content='ISCRIZIONE ALLE NEWSLETTER' />
       <Modal.Content>
-       <div class="grouped fields">
-	   <div class="field">
-	   <div class="ui checkbox">
-      <input type="checkbox"/>
-      <label>Le Notizie del Portale</label>
+       <div className="grouped fields">
+	   <div className="field">
+	   <div className="ui checkbox">
+      <Checkbox
+	  label="Le Notizie del Portale"
+	  value="ch1"
+      checked = {ch1}
+	  //onClick={(data)=>this.onChangeCheckbox(ch1)}
+	  />
     </div>
 	</div>
-	 <div class="field">
-	<div class="ui checkbox">
-      <input type="checkbox" change="{!c.onCheck}"/>
-      <label>Gli eventi e le Notizie della Cultura</label>
+	 <div className="field">
+	<div className="ui checkbox">
+      <Checkbox
+	  label="Gli eventi e le Notizie della Cultura"
+	  name="ch2"
+	  checked = {ch2}
+	  onClick={(data)=>this.onChangeCheckbox(ch2)}
+	  />
     </div>
 	</div>
-	 <div class="field">
-	<div class="ui checkbox">
-      <input type="checkbox" change="{!c.onCheck}"/>
-      <label>Gli eventi e le notizie del centro per le famiglie</label>
+	 <div className="field">
+	<div className="ui checkbox">
+      <Checkbox 
+	  label="Gli eventi e le notizie del centro per le famiglie"
+      name="ch3"
+	  checked = {ch3}
+	  onClick={(data)=>this.onChangeCheckbox(ch3)}
+	  />
     </div>
 	</div>
 	</div>
       </Modal.Content>
       <Modal.Actions>
 	    <Message
-      success
-      header='Form Completed'
-      content="You're all signed up for the newsletter"
+      visible
+      content="Select your prefered newsletters"
     />
         <Button color='blue' onClick={() => setOpen(false)}>
           <Icon name='checkmark' /> Subscribe
         </Button>
 		 <Button color='red' onClick={() => setOpen(false)}>
-          <Icon name='x iconx' /> Anulla
+          <Icon name='x' /> Anulla
         </Button>
       </Modal.Actions>
     </Modal>
-  )
+	</div>
+  );
 }
 
 export default PopUp
+	
+
+
